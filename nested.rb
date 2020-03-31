5049ab1a694945e334a5c70d322e8438c9c51dd1
Elena Dinca <elenadinca25@gmail.com>
2020-03-31 22:30:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -35,9 +35,10 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for][0]
+
 programmer_hash[:grace_hopper]
 programmer_hash[:dennis_ritchie]["C"]
+alan_kay_is_known_for
 end
 
 def dennis_ritchies_language
