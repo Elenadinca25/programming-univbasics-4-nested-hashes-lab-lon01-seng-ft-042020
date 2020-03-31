286d748edf7a3583066c538f88a401de62e85d82
Elena Dinca <elenadinca25@gmail.com>
2020-03-31 22:23:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,6 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-programmer_hash[:alan_kay]["Object Orientation"]
 	programmer_hash =
  		{
       :grace_hopper => {
@@ -37,7 +36,7 @@ programmer_hash[:alan_kay]["Object Orientation"]
       }
     }
 
-
+programmer_hash[:grace_hopper]
 programmer_hash[:dennis_ritchie]["C"]
 end
 
