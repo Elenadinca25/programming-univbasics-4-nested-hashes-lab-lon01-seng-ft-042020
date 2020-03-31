849c8ad96e216f574f2530cf6853e7286dea4072
Elena Dinca <elenadinca25@gmail.com>
2020-03-31 23:28:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,13 +61,13 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-:yukihiro_matsumoto => {
-    :known_for => "Ruby",
-    :languages => ["LISP", "C"]
- }
- return the entire updated hash
+
 
 	programmer_hash =
+  :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+   },
  		{
       :grace_hopper => {
         :known_for => "COBOL",
@@ -82,7 +82,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+return the entire updated hash
 end
 
 def changing_alan
